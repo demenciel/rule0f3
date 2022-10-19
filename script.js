0c32f6c757f0ae8109576f966778315da8ea9efd
Alex Couture <79432799+demenciel@users.noreply.github.com>
2022-10-19 15:12:14 -0400

    Update Taxes

--- script.js
+++ script.js
@@ -6,11 +6,12 @@
 var result = document.querySelector('#result');
 
 
-$(huile).keyup(function()  {
+
+$(meche).keyup(function()  {
     var moulePrice = parseFloat($(moule).val());
-    var cirePrice = parseFloat($(cire).val()) * 150 / 45;
-    var mechePrice = parseFloat($(meche).val()) * 7.40 / 100;
-    var huilePrice = parseFloat($(huile).val());
+    var cirePrice = parseFloat($(cire).val()) * 159.69 / 45;
+    var mechePrice = parseFloat($(meche).val()) * 80.09 / 100;
+    var huilePrice = parseFloat($(huile).val()) * 23.09 / 250;
 
     var resultPrice = moulePrice + cirePrice + mechePrice + huilePrice;
     var resultRounded = Math.round(resultPrice * 100) / 100;
